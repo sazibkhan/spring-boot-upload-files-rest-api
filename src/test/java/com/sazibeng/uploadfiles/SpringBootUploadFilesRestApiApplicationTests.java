7de435865a0eea619b6b569727594ee9808f05a1
sazibkhan <sazibkhancse@gmail.com>
2024-03-09 13:37:44 +0600

    Spring Boot Upload Files Rest Api

---
+++ src/test/java/com/sazibeng/uploadfiles/SpringBootUploadFilesRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sazibeng.uploadfiles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootUploadFilesRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
